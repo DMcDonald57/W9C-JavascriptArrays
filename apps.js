80c51f9ca685fe19b8c414f5f0491a7389dc7303
DMcDonald57 <d.mcdonald579@gmail.com>
2022-11-03 20:31:50 -0600

    added Arrays, variables and push code

---
+++ apps.js
@@ -0,0 +1,26 @@
+// Creating Arrays of Items, price and stocking
+let itemNames = ["Book", "DVD", "Blueray"]
+console.log(itemNames);
+
+let itemPrice = ["$6.00", "$15.00", "$22.00"]
+console.log(itemPrice);
+
+let inStock = ["Yes", "No", "Soon"]
+console.log(inStock);
+
+// Creating two variables for item and price
+
+let firstItemname = "Book";
+let firstItemprice = "$6.00";
+console.log();
+
+// adding new item, price and stocking info to Arrays
+
+itemNames.push ("Game")
+console.log(itemNames)
+
+itemPrice.push ("$79.00")
+console.log(itemPrice)
+
+inStock.push ("Next Year")
+console.log(inStock);
