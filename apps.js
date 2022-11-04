07c128504c81723a17350dd474ed833c8b2e2ce8
DMcDonald57 <d.mcdonald579@gmail.com>
2022-11-03 21:26:49 -0600

    Added pop variables having last items removed

--- apps.js
+++ apps.js
@@ -5,14 +5,15 @@ console.log(itemNames);
 let itemPrice = ["$6.00", "$15.00", "$22.00"]
 console.log(itemPrice);
 
-let inStock = ["Yes", "No", "Soon"]
+let inStock = ["Yes", "Yes", "No"]
 console.log(inStock);
 
 // Creating two variables for item and price
 
-let firstItemname = "Book";
-let firstItemprice = "$6.00";
-console.log();
+let firstItemname = itemNames[0];
+let firstItemprice = itemPrice[0];
+console.log(firstItemname)
+console.log(firstItemprice)
 
 // adding new item, price and stocking info to Arrays
 
@@ -22,5 +23,14 @@ console.log(itemNames)
 itemPrice.push ("$79.00")
 console.log(itemPrice)
 
-inStock.push ("Next Year")
+inStock.push ("No")
 console.log(inStock);
+
+// Variables removing items using Pop
+
+let lastItemnamed = itemNames.pop();
+console.log(lastItemnamed);
+let lastItemprice = itemPrice.pop();
+console.log(lastItemprice);
+let lastItemstock = inStock.pop();
+console.log(lastItemstock);
